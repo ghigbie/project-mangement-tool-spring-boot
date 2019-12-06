73aaf1df8242c25ea85e4be13b8758a182cab1cc
ghigbie <gehigbie@gmail.com>
2019-12-05 19:17:10 -0700

    Add new getters and setters and constructors

--- src/main/java/com/georgehigbie/projectmanagement/entities/Employee.java
+++ src/main/java/com/georgehigbie/projectmanagement/entities/Employee.java
@@ -11,9 +11,41 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long employeeId;
+    private String firstName;
     private String lastName;
     private String email;
 
+    public Employee(){
 
+    }
 
+    public Employee(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
