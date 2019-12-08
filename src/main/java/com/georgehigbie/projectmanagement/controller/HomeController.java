e76dfb5a833166fab29f7d689ba5f1ec05a89ce1
ghigbie <gehigbie@gmail.com>
2019-12-07 17:34:43 -0700

    Edit controller

--- src/main/java/com/georgehigbie/projectmanagement/controller/HomeController.java
+++ src/main/java/com/georgehigbie/projectmanagement/controller/HomeController.java
@@ -1,10 +1,13 @@
 package com.georgehigbie.projectmanagement.controller;
 
 import com.georgehigbie.projectmanagement.dao.ProjectRepository;
+import com.georgehigbie.projectmanagement.entities.Project;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -14,6 +17,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String displayHome(Model model){
-
+        List<Project> projects = proRepo.findAll();
+        return "home";
     }
 }
