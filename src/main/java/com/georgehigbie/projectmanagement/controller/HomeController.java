0ee8b0b1fb3c458037446156022312b75e33de9c
ghigbie <gehigbie@gmail.com>
2019-12-07 17:31:16 -0700

    Edit controller

--- src/main/java/com/georgehigbie/projectmanagement/controller/HomeController.java
+++ src/main/java/com/georgehigbie/projectmanagement/controller/HomeController.java
@@ -1,8 +1,19 @@
 package com.georgehigbie.projectmanagement.controller;
 
+import com.georgehigbie.projectmanagement.dao.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    ProjectRepository proRepo;
+
+    @GetMapping("/")
+    public String displayHome(Model model){
+
+    }
 }
