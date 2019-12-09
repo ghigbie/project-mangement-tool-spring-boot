fd7b2f46b4ba348f38d2f2db9f0ce5e3cf12d154
ghigbie <gehigbie@gmail.com>
2019-12-08 20:15:41 -0700

    Add table classes

--- src/main/java/com/georgehigbie/projectmanagement/entities/Project.java
+++ src/main/java/com/georgehigbie/projectmanagement/entities/Project.java
@@ -1,9 +1,7 @@
 package com.georgehigbie.projectmanagement.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Project {
@@ -14,6 +12,8 @@ public class Project {
     private String name;
     private String stage;
     private String description;
+
+    private List<Employee> employess;
 
     public Project(){
 
